0a4bb5ae12c3b38ccf41a4c4baf45ea4cbe22e2d
ainaasyhda <48386586+ainaasyhda@users.noreply.github.com>
2019-03-20 17:28:11 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,61 +1,33 @@
-import java.net.*; 
-import java.io.*; 
-  
-public class Server 
-{ 
-    //initialize socket and input stream 
-    private Socket          socket   = null; 
-    private ServerSocket    server   = null; 
-    private DataInputStream in       =  null; 
-  
-    // constructor with port 
-    public Server(int port) 
-    { 
-        // starts server and waits for a connection 
-        try
-        { 
-            server = new ServerSocket(port); 
-            System.out.println("Server begin"); 
-  
-            System.out.println("Assalamualaikum !!"); 
-  
-            socket = server.accept(); 
-            System.out.println("Waalaikumsalam !!"); 
-  
-            // takes input from the client socket 
-            in = new DataInputStream( 
-                new BufferedInputStream(socket.getInputStream())); 
-  
-            String line = ""; 
-  
-            // reads message from client until "Over" is sent 
-            while (!line.equals("Over")) 
-            { 
-                try
-                { 
-                    line = in.readUTF(); 
-                    System.out.println(line); 
-  
-                } 
-                catch(IOException i) 
-                { 
-                    System.out.println(i); 
-                } 
-            } 
-            System.out.println("Close connection"); 
-  
-            // close connection 
-            socket.close(); 
-            in.close(); 
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
-    } 
-  
-    public static void main(String args[]) 
-    { 
-        Server server = new Server(6789); 
-    } 
-} 
+import java.io.*;
+import java.net.*;
+public class server
+{
+public static void main(String[] args)throws Exception
+{
+ServerSocket serversocket = new ServerSocket(8080);
+System.out.println("Server dah sedia ni!");
+Socket sock = serversocket.accept();
+System.out.println("Waalaikummussalam");
+
+BufferedReader keyRead = new BufferedReader(new 
+InputStreamReader(System.in));
+OutputStream ostream = sock.getOutputStream();
+PrintWriter pwrite = new PrintWriter(ostream,true);
+
+InputStream istream = sock.getInputStream();
+BufferedReader receiveRead = new BufferedReader(new 
+InputStreamReader(istream));
+
+String receiveMessage,sendMessage;
+while(true)
+{
+if((receiveMessage=receiveRead.readLine())!=null)
+{
+System.out.println(receiveMessage);
+}
+sendMessage = keyRead.readLine();
+pwrite.println(sendMessage);
+pwrite.flush();
+}
+}
+}
