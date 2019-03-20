bbc7ac8066626d7530fd8dce96190aca0ef623f2
ainaasyhda <48386586+ainaasyhda@users.noreply.github.com>
2019-03-20 10:28:15 +0800

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,61 @@
+import java.net.*; 
+import java.io.*; 
+  
+public class Server 
+{ 
+    //initialize socket and input stream 
+    private Socket          socket   = null; 
+    private ServerSocket    server   = null; 
+    private DataInputStream in       =  null; 
+  
+    // constructor with port 
+    public Server(int port) 
+    { 
+        // starts server and waits for a connection 
+        try
+        { 
+            server = new ServerSocket(port); 
+            System.out.println("Server begin"); 
+  
+            System.out.println("Waiting For Client...."); 
+  
+            socket = server.accept(); 
+            System.out.println("yeayy!! Connected successful."); 
+  
+            // takes input from the client socket 
+            in = new DataInputStream( 
+                new BufferedInputStream(socket.getInputStream())); 
+  
+            String line = ""; 
+  
+            // reads message from client until "Over" is sent 
+            while (!line.equals("Over")) 
+            { 
+                try
+                { 
+                    line = in.readUTF(); 
+                    System.out.println(line); 
+  
+                } 
+                catch(IOException i) 
+                { 
+                    System.out.println(i); 
+                } 
+            } 
+            System.out.println("Close connection"); 
+  
+            // close connection 
+            socket.close(); 
+            in.close(); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        Server server = new Server(6789); 
+    } 
+} 
