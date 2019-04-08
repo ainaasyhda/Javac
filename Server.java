a8ca15b2edd00d4de7ad11d1d9a6757fe7b66a48
ainaasyhda <48386586+ainaasyhda@users.noreply.github.com>
2019-04-08 21:57:29 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,33 +1,183 @@
-import java.io.*;
-import java.net.*;
-class Server
-{
-public static void main(String[] args)throws Exception
-{
-ServerSocket serversocket = new ServerSocket(5555);
-System.out.println("Server dah sedia ni!");
-Socket sock = serversocket.accept();
-System.out.println("Waalaikummussalam");
+#include <stdio.h>
 
-BufferedReader keyRead = new BufferedReader(new 
-InputStreamReader(System.in));
-OutputStream ostream = sock.getOutputStream();
-PrintWriter pwrite = new PrintWriter(ostream,true);
 
-InputStream istream = sock.getInputStream();
-BufferedReader receiveRead = new BufferedReader(new 
-InputStreamReader(istream));
 
-String receiveMessage,sendMessage;
-while(true)
+#include <sys/types.h> 
+
+
+
+#include <sys/socket.h>
+
+
+
+#include <string.h>
+
+
+
+#include <stdlib.h>
+
+
+
+#include <netinet/in.h>
+
+
+
+#include <unistd.h> 
+
+
+
+int main( int argc, char *argv[] )
+
+
+
 {
-if((receiveMessage=receiveRead.readLine())!=null)
-{
-System.out.println(receiveMessage);
-}
-sendMessage = keyRead.readLine();
-pwrite.println(sendMessage);
-pwrite.flush();
-}
-}
+
+	// Variables 
+
+	int sock ;
+
+	struct sockaddr_in server;
+
+	int mysock;
+
+	char buff [1024];
+
+	int rval;
+
+	char server_message[256] ;
+
+
+
+	// Create Socket
+
+	sock = socket(AF_INET, SOCK_STREAM , 0);
+
+	if(sock < 0) {
+
+		perror("Failed to create socket");
+
+		exit(1);
+
+	}
+
+
+
+
+
+	server.sin_family = AF_INET;
+
+	server.sin_addr.s_addr = INADDR_ANY;
+
+	server.sin_port =htons(22000);
+
+
+
+	// Call Bind 
+
+	if(bind(sock, (struct sockaddr *)&server, sizeof(server)))
+
+	{
+
+		perror("Bind Failed");
+
+		exit(1);
+
+	}
+
+
+
+	// Listen
+
+	listen(sock , 10);
+
+
+
+	// Accept
+
+
+
+	do{
+
+		mysock = accept(sock , (struct sockaddr *) 0, 0);	
+
+			 
+
+	        Previous:
+
+	 
+
+		if(mysock == -1)
+
+		{
+
+			perror("accept failed");
+
+		}
+
+		else
+
+		{
+
+
+
+		memset(buff, 0, sizeof(buff));
+
+
+
+		if((rval = recv(mysock, buff, sizeof(buff), 0)) < 0)
+
+		{ perror("reading stream message failed");    }
+
+		
+
+		else if (rval == 0)
+
+		{ printf("Ending connection");  }
+
+
+
+		else 
+
+  		{
+
+
+
+        	 recv(mysock, buff, sizeof(buff), 1);
+
+		 printf("From Client: %s", buff);
+
+		 recv(mysock, buff, sizeof(buff), 0);
+
+		 printf("%s",buff);
+
+		 printf("\n");
+
+		 
+
+		//goto Previous; 
+
+		
+
+		scanf ("%[^\n]%*c", server_message);
+
+			 
+
+                send(mysock,server_message,strlen(server_message),0);
+
+		}
+
+		
+
+		close(mysock);
+
+		}
+
+	}while(1);
+
+
+
+	return 0;
+
+
+
 }
